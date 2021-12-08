812e3370b1afdffd977da78a23623213ddc49a8b
Islom <nolove02@bk.ru>
2021-12-09 01:08:22 +0500

    menu search

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from customer.views import Index, About, Order
+from customer.views import Index, About, Order, Menu, MenuSearch
 
 app_name = "customer"
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", Index.as_view(), name="index"),
     path("about/", About.as_view(), name="about"),
     path("order/", Order.as_view(), name="order"),
+    path("menu/", Menu.as_view(), name="menu"),
+    path("menu/search/", MenuSearch.as_view(), name="menu-search"),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,9 @@
 from django.core.mail import send_mail
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+
 from .models import MenuItem, Category, OrderModel
 
 
@@ -93,3 +96,26 @@ class Order(View):
         }
 
         return render(request, 'customer/order_confirmation.html', context)
+
+
+class Menu(View):
+    def get(self, request, *args, **kwargs):
+        menu = MenuItem.objects.all()
+
+        context = {
+            "menu_items": menu,
+        }
+
+        return render(request, "customer/menu.html", context)
+
+
+class MenuSearch(View):
+    def get(self, request, *args, **kwargs):
+        q = self.request.GET.get("q")
+        menu = MenuItem.objects.filter(
+                Q(name__icontains=q) | Q(price__icontains=q) | Q(description__icontains=q)
+            )
+        context = {
+            "menu_items": menu,
+        }
+        return render(request, "customer/menu.html", context)
